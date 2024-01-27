e35e8d0090e6b29f4b88cc35718411437611013d
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2024-01-27 20:46:59 +0300

    cucumber applied to project

---
+++ src/test/java/step_definitions/BaseStep.java
@@ -0,0 +1,7 @@
+package step_definitions;
+
+import utils.Pages;
+
+public abstract class BaseStep{
+   protected final Pages PAGES = new Pages();
+}
---
+++ src/test/java/step_definitions/Draggable_AutoScrolling_Step.java
@@ -0,0 +1,90 @@
+package step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.assertj.core.api.Assertions;
+import io.cucumber.java.en.Given;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 1. Open the browser and navigate to the page 2. click on the draggable button 3. click
+ * on the auto scrolling button 4. Drag the first draggable element by 215 pixels to the
+ * right 5. Drag the third draggable element by -215 pixels to the left 6. Verify that the
+ * elements are ordered as "Three", "Two", "One"
+ */
+public class Draggable_AutoScrolling_Step extends BaseStep {
+	private static final Logger LOGGER = LogManager.getLogger(Draggable_AutoScrolling_Step.class);
+
+	@Given("The user is on the webautomation page")
+	public void theUserIsOnTheWebAutomationPage(){
+		PAGES.getHomePage().clickWebAutomationLink();
+		LOGGER.debug("Open the browser and navigate to the webautomation page");
+	}
+	@And("the user clicked to Draggable tab")
+	public void theUserClickedToDraggableTab(){
+		PAGES.getWebAutomationPage().clickOnDraggableLink();
+		LOGGER.debug("Click on the draggable button");
+	}
+	@And("the user clicked to Auto Scrolling link")
+	public void theUserClickedToAutoScrollingLink(){
+		PAGES.getWebAutomationPage().clickOnAutoScrollingLink();
+		LOGGER.debug("Click on the auto scrolling button");
+	}
+	@When("the user Drag the first draggable element by 215 pixels to the right")
+	public void theUserDragFirstDraggableElement(){
+		PAGES.getAutoScrollingPage().dragFirstDraggableByXOffset(215);
+		LOGGER.debug("Drag the first draggable element by 215 pixels to the right");
+	}
+
+	@And("the user Drag the third draggable element by -215 pixels to the left")
+	public void theUserDragThirdDraggableElement(){
+		PAGES.getAutoScrollingPage().dragThirdDraggableByXOffset(-215);
+		LOGGER.debug("Drag the third draggable element by -215 pixels to the left");
+	}
+
+	@Then("the elements are ordered as Three, Two, One")
+	public void verifyOrderOfElements(){
+		int finalX1 = PAGES.getAutoScrollingPage().getLocationOfFirstDraggableX();
+		int finalX2 = PAGES.getAutoScrollingPage().getLocationOfSecondDraggableX();
+		int finalX3 = PAGES.getAutoScrollingPage().getLocationOfThirdDraggableX();
+
+		Assertions.assertThat(finalX3).isLessThan(finalX2);
+		Assertions.assertThat(finalX2).isLessThan(finalX1);
+		LOGGER.debug("Verify that the elements are ordered as \"Three\", \"Two\", \"One\"");
+	}
+
+//	void testAutoScrolling() {
+//		// 1. Open the browser and navigate to the page
+//		pages.getHomePage().clickWebAutomationLink();
+//		LOGGER.debug("Open the browser and navigate to the page");
+//
+//		// 2. click on the draggable button
+//		pages.getWebAutomationPage().clickOnDraggableLink();
+//		LOGGER.debug("Click on the draggable button");
+//
+//		// 3. click on the auto scrolling button
+//		pages.getWebAutomationPage().clickOnAutoScrollingLink();
+//		LOGGER.debug("Click on the auto scrolling button");
+//
+//		// 4. Drag the first draggable element by 215 pixels to the right
+//		pages.getAutoScrollingPage().dragFirstDraggableByXOffset(215);
+//		LOGGER.debug("Drag the first draggable element by 215 pixels to the right");
+//
+//		// 5. Drag the third draggable element by -215 pixels to the left
+//		pages.getAutoScrollingPage().dragThirdDraggableByXOffset(-215);
+//		LOGGER.debug("Drag the third draggable element by -215 pixels to the left");
+//
+//		// 6. Verify that the elements are ordered as "Three", "Two", "One"
+//		int finalX1 = pages.getAutoScrollingPage().getLocationOfFirstDraggableX();
+//		int finalX2 = pages.getAutoScrollingPage().getLocationOfSecondDraggableX();
+//		int finalX3 = pages.getAutoScrollingPage().getLocationOfThirdDraggableX();
+//
+//		Assertions.assertThat(finalX3).isLessThan(finalX2);
+//		Assertions.assertThat(finalX2).isLessThan(finalX1);
+//		LOGGER.debug("Verify that the elements are ordered as \"Three\", \"Two\", \"One\"");
+//
+//	}
+
+}
